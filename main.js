0f71451f5d654e8a23f4d1141d08014cc8830a89
mariano <marianotorresleyva@gmail.com>
2022-12-07 22:40:57 -0500

    Step 1: Get Text - Done

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const puppeteer = require("puppeteer");
+import puppeteer from 'puppeteer';
+
 (async () => {
-    const browser = await puppeteer.launch({
-        executablePath: "/opt/google/chrome/./chrome",
-    });
-    const page = await browser.newPage();
-    await page.goto("https://ed.team/cursos/python");
-    await page.screenshot({ path: "image.png" });
-    await browser.close();
+  const BROWSER_LOCATION = "/usr/bin/google-chrome"
+  const browser = puppeteer.launch({executablePath:BROWSER_LOCATION})
+  const page = await browser.newPage();
+  await page.goto('https://app.aluracursos.com/loginForm?urlAfterLogin=https://app.aluracursos.com/dashboardhttps://developers.google.com/web/');
+  // await page.
+  await browser.close();
 })();
